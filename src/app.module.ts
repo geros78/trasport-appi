f46186a6d9d44dce08bcb87ec5d3d32ba722516e
Alejandro Otaiza Orellano <alejo.o.o1000@gmail.com>
2022-05-01 12:50:12 -0500

    Agregador los controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RidersModule } from './riders/riders.module';
+import { UsersModule } from './users/users.module';
+import { CountriesModule } from './countries/countries.module';
+import { CityModule } from './city/city.module';
+import { UserAddressModule } from './user_address/user_address.module';
+import { DriversModule } from './drivers/drivers.module';
+import { DriverLicensesModule } from './driver_licenses/driver_licenses.module';
+import { LicenseTypesModule } from './license_types/license_types.module';
 
 @Module({
-  imports: [],
+  imports: [RidersModule, UsersModule, CountriesModule, CityModule, UserAddressModule, DriversModule, DriverLicensesModule, LicenseTypesModule],
   controllers: [AppController],
   providers: [AppService],
 })
